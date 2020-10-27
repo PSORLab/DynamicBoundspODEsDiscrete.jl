9fcb3f2f7c47c8bb744729cff6b88948e60f9ab9
Matt W <matthew.wilhelm@uconn.edu>
2020-10-27 10:26:14 -0400

    get_val_local to get_val_loc

--- src/DiscretizeRelax/utilities/access_functions.jl
+++ src/DiscretizeRelax/utilities/access_functions.jl
@@ -317,13 +317,13 @@ function DBB.getall(t::DiscretizeRelax{X,T}, ::DBB.Subgradient{Upper}) where {X,
 end
 
 function DBB.get(out::Vector{Float64}, t::DiscretizeRelax{X,T}, v::DBB.Bound{Lower}) where {X, T <: AbstractInterval}
-    val_loc = get_val_loc_local(t, v.index, v.time)
+    val_loc = get_val_loc(t, v.index, v.time)
     @__dot__ out = lo(t.storage[val_loc])
     return
 end
 
 function DBB.get(out::Vector{Float64}, t::DiscretizeRelax{X,T}, v::DBB.Bound{Upper}) where {X, T <: AbstractInterval}
-    val_loc = get_val_loc_local(t, v.index, v.time)
+    val_loc = get_val_loc(t, v.index, v.time)
     @__dot__ out = hi(t.storage[val_loc])
     return
 end
@@ -389,25 +389,25 @@ function DBB.getall(t::DiscretizeRelax{X,T}, ::DBB.Bound{Upper}) where {X, T <: 
 end
 
 function DBB.get(out::Vector{Float64}, t::DiscretizeRelax{X,T}, v::DBB.Relaxation{Lower}) where {X, T <: MC}
-    val_loc = get_val_loc_local(t, v.index, v.time)
+    val_loc = get_val_loc(t, v.index, v.time)
     @__dot__ out = cv(t.storage[val_loc])
     return
 end
 
 function DBB.get(out::Vector{Float64}, t::DiscretizeRelax{X,T}, v::DBB.Relaxation{Upper}) where {X, T <: MC}
-    val_loc = get_val_loc_local(t, v.index, v.time)
+    val_loc = get_val_loc(t, v.index, v.time)
     @__dot__ out = cc(t.storage[val_loc])
     return
 end
 
 function DBB.get(out::Vector{Float64}, t::DiscretizeRelax{X,T}, v::DBB.Relaxation{Lower}) where {X, T <: AbstractInterval}
-    val_loc = get_val_loc_local(t, v.index, v.time)
+    val_loc = get_val_loc(t, v.index, v.time)
     @__dot__ out = lo(t.storage[val_loc])
     return
 end
 
 function DBB.get(out::Vector{Float64}, t::DiscretizeRelax{X,T}, v::DBB.Relaxation{Upper}) where {X, T <: AbstractInterval}
-    val_loc = get_val_loc_local(t, v.index, v.time)
+    val_loc = get_val_loc(t, v.index, v.time)
     @__dot__ out = hi(t.storage[val_loc])
     return
 end
